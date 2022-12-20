b9f1e82792b4a67fb8d9853fb7987bd757f1f049
FaroukAhmad <farouqgt@gmail.com>
2022-12-20 01:47:37 -0800

    my commit exception handling in python

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_length):
+    for i in range(0,list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
+        except TypeError:
             print("wrong type")
             result = 0
         except ZeroDivisionError:
