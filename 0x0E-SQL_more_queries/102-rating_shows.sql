0de6c48f93f7ffa5cdf3ec409a920289830c1210
Umar Farouk <78317501+FaroukAhmad@users.noreply.github.com>
2023-02-22 12:04:17 +0100

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,19 +1,8 @@
--- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
--- Records are ordered by ascending show title.
-SELECT DISTINCT `title`
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON s.`show_id` = t.`id`
-
-       LEFT JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE t.`title` NOT IN
-             (SELECT `title`
-                FROM `tv_shows` AS t
-	             INNER JOIN `tv_show_genres` AS s
-		     ON s.`show_id` = t.`id`
-
-		     INNER JOIN `tv_genres` AS g
-		     ON g.`id` = s.`genre_id`
-		     WHERE g.`name` = "Comedy")
- ORDER BY `title`;
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
